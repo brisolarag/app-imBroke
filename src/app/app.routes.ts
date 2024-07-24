fe84a6ea7ad1646d7e5d6de25b66d4615a9cf0e7
Gabriel Brisolara <dev.brisolara@gmail.com>
2024-07-24 13:53:51 -0300

    refactor: vendors template scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './sections/main/home/home.component';
 
 export const routes: Routes = 
 [
     {path: 'signin', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
+
+    {path: 'home', component: HomeComponent},
 ];
